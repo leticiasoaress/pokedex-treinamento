3a226e66749c8b62e1ad1f23a07d2b6c97722584
leticiasoaress <leticia.soares@acerto.com.br>
2023-01-16 11:35:12 -0300

    Configuração classe startup.

---
+++ src/Pokedex.Api/Startup.cs
@@ -0,0 +1,31 @@
+﻿namespace Pokedex.Api;
+
+public class Startup
+{
+    private readonly IConfiguration _configuration;
+    private readonly IServiceCollection _services;
+
+    public Startup(WebApplicationBuilder builder)
+    {
+        this._configuration = builder.Configuration;
+        this._services = builder.Services;
+    }
+
+    public void ConfigureServices()
+    {
+        this._services.AddControllers();
+        this._services.AddEndpointsApiExplorer();
+        this._services.AddSwaggerGen();
+    }
+
+    public void ConfigureApplication(IApplicationBuilder app)
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+
+        app.UseHttpsRedirection();
+        app.UseRouting();
+        app.UseAuthorization();
+        app.UseEndpoints(options => options.MapControllers());
+    }
+}
